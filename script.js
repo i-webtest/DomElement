fb7e3800d25947e3b1f360411ad70e10cbaf58c9
i-webtest <stasbass80@gmail.com>
2022-03-16 23:32:24 +0300

    добавил комментарии

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+//функция-конструктор DomElement со свойствами selector, height, width, bg, fontSize
 const DomElement = function (selector, height, width, bg, fontSize) {
   this.selector = selector;
   this.height = height;
@@ -7,13 +8,16 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   this.bg = bg;
   this.fontSize = fontSize;
 
+  //метод, который создает элемент на странице в зависимости от условия:
   this.createElement = function () {
     let element;
     let elementClass = this.selector.slice(1);
+    //если строка selector начинается с точки, создаем div с классом
     if (this.selector[0] === ".") {
       element = document.createElement("div");
       element.classList.add(elementClass);
       element.innerText = "Hello, world!!!";
+      //если строка selector начинается с решетки # то создаем параграф с id
     } else if (this.selector[0] === "#") {
       element = document.createElement("p");
       element.setAttribute("id", elementClass);
@@ -22,6 +26,7 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 
     document.body.append(element);
 
+    //с помощью cssText задал стили
     element.style.cssText =
       "height:" +
       this.height +
@@ -45,8 +50,10 @@ const DomElement = function (selector, height, width, bg, fontSize) {
   };
 };
 
+//Создал новый объект на основе класса DomElement
 const createNewObject1 = new DomElement(".block", 100, 300, "red", 30);
 const createNewObject2 = new DomElement("#best", 100, 200, "blue", 30);
 
+//Вызвал его метод чтобы создать элемент на странице
 createNewObject1.createElement();
 createNewObject2.createElement();
